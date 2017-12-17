de3d1e439811b0841a5a96a9371c6d1e0c5b254c
nafyone <31994111+nafyone@users.noreply.github.com>
2017-12-18 01:48:16 +0800

    Update sftpprmk1.py

--- sftpprmk1.py
+++ sftpprmk1.py
@@ -1,11 +1,11 @@
 import os
 import paramiko 
+
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('fe80::20c:29ff:fe44:7677%eth0', username="abc", password="1212")
+
 sftp = ssh.open_sftp()
-localpath = 'test1.txt'
-remotepath = 'test.txt'
 sftp.get('test.txt', 'download.txt')
 sftp.close()
 ssh.close()
